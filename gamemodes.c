2c1013863c0eccf03bd89d915a685f7aeb60a503
Muhammad Ahmad <novahat2580@gmail.com>
2025-06-15 17:47:37 +0500

    Update gamemodes.c

--- gamemodes.c
+++ gamemodes.c
@@ -18,7 +18,7 @@ void loadLeaderboard() {
     
     leaderboardCount = 0;
     while (leaderboardCount < MAX_LEADERBOARD && 
-           !feof(file) &&   fscanf(file, "%50s %d %d %9s", leaderboard[leaderboardCount].name, &leaderboard[leaderboardCount].money,
+ fscanf(file, "%50s %d %d %9s", leaderboard[leaderboardCount].name, &leaderboard[leaderboardCount].money,
 &leaderboard[leaderboardCount].rounds,
 leaderboard[leaderboardCount].status) == 4)
  {
